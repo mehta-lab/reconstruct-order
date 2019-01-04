1dc8eb07fea6969b875c044d55aa5d2dddd0f3c0
sguo <symguo@gmail.com>
2019-01-03 23:24:12 -0800

    bug fix

--- birefringence_python/PolScope/multiDimProcess.py
+++ birefringence_python/PolScope/multiDimProcess.py
@@ -162,8 +162,8 @@ def loopT(img_io, img_reconstructor, flatField=False, bgCorrect=True, circularit
         img_io.tIdx = tIdx
         if img_io.loopZ =='sample':
             if img_io.bg_method == 'Local_defocus':
-                print('compute defocused backgorund at {} ...'.format(img_io_bg.posIdx))
                 img_io_bg = img_io.bg_local
+                print('compute defocused backgorund at {} ...'.format(img_io_bg.posIdx))
                 img_io_bg.tIdx = tIdx
                 img_io_bg.zIdx = 0
                 ImgRawBg, ImgProcBg, ImgFluor, ImgBF = parse_tiff_input(img_io_bg)  # 0 for z-index
