e422758157d10e541003762541ef7800a271fcf7
Bryant <bchhun@gmail.com>
2019-01-08 11:29:57 -0800

    Default Changelist
    Former-commit-id: c793ab1068e5533c9c8b283b8d6b4a42a9052e73 [formerly 9a3563ef258e8bd45cd163964e7028433280edca]
    Former-commit-id: fbadf78c3097f2920b0fbed9a4c2bda592eb36fe

--- tests/testReconOrder_mse.py
+++ tests/testReconOrder_mse.py
@@ -13,7 +13,8 @@ import yaml
 import cv2
 
 from compute.multiDimProcess import findBackground
-from compute.reconstruct import ImgReconstructor=
+from compute.reconstruct import ImgReconstructor
+from utils.imgIO import parse_tiff_input
 from tests.testMetrics import mse
 
 
@@ -39,88 +40,62 @@ class TestImageReconstruction(unittest.TestCase):
                          condition + \
                          "/img_Scattering_t000_p000_z000.tif"
 
+    source_config_file = './TestData/CONFIG_SM_2018_1002_1633_1.yml'
     sourceData = "./testData/rawData/2018_10_02_MouseBrainSlice/"
     sourceSample = "SM_2018_1002_1633_1/"
     sourceBackground = "BG_2018_1002_1625_1/"
 
     RawDataPath = sourceData+sourceSample
 
-    def __init__(self):
-        #read config
-        with open(config_fname, 'r') as f:
-            config = yaml.load(f)
+    def __init__(self, *args, **kwargs):
+        super(TestImageReconstruction, self).__init__(*args, **kwargs)
 
+        with open('./TestData/CONFIG_SM_2018_1002_1633_1.yml', 'r') as f:
+            config = yaml.load(f)
+        self.RawDataPath = config['dataset']['RawDataPath']
+        self.ProcessedPath = config['dataset']['ProcessedPath']
+        self.ImgDir = config['dataset']['ImgDir']
+        self.SmDir = config['dataset']['SmDir']
+        self.BgDir = config['dataset']['BgDir']
+        self.BgDir_local = config['dataset']['BgDir_local']
+        self.outputChann = config['processing']['outputChann']
+        self.circularity = config['processing']['circularity']
+        self.bgCorrect = config['processing']['bgCorrect']
+        self.flatField = config['processing']['flatField']
+        self.batchProc = config['processing']['batchProc']
+        self.norm = config['plotting']['norm']
 
     def construct_all(self):
-        #read config
-        imgSm, img_reconstructor = findBackground(self.RawDataPath, ProcessedPath, ImgDir, SmDir, BgDir, outputChann,
-                           BgDir_local=BgDir_local, flatField=flatField,bgCorrect=bgCorrect,
+        self.img_io, img_reconstructor = findBackground(self.RawDataPath, self.ProcessedPath, self.ImgDir, self.SmDir, self.BgDir, self.outputChann,
+                           BgDir_local=self.BgDir_local, flatField=self.flatField,bgCorrect=self.bgCorrect,
                            ff_method='open')
+        self.img_io.posIdx = 0
+        self.img_io.tIdx = 0
+        self.img_io.zIdx = 0
+        ImgRawSm, ImgProcSm, ImgFluor, ImgBF = parse_tiff_input(self.img_io)
+        img_stokes_sm = img_reconstructor.compute_stokes(ImgRawSm)
+        img_computed_sm = img_reconstructor.reconstruct_birefringence(img_stokes_sm, self.img_io.param_bg,
+                                                                      circularity=self.circularity,
+                                                                      bg_method=self.img_io.bg_method,
+                                                                      extra=False)  # background subtraction
+        [self.I_trans, self.retard, self.azimuth, self.polarization] = img_computed_sm
 
-        # create file loaders
-        datapipe = PipeToReconOrder(type="Test", sample_type="Sample1")
-        datapipe_bg = PipeToReconOrder(type="Test", sample_type='BG')
-
-        # initialize processors
-        self.processor = ReconOrder()
-        self.processor_bg = ReconOrder()
-        self.processor.frames = 5
-        self.processor_bg.frames = 5
-
-        datapipe.set_processor(self.processor)
-        datapipe_bg.set_processor(self.processor_bg)
-
-        datapipe.compute_inst_matrix()
-        datapipe_bg.compute_inst_matrix()
-
-        # BGprocess first
-        datapipe_bg.run_reconstruction()
-        datapipe.run_reconstruction_BG_correction(datapipe_bg.get_processor())
-
-    def construct_BG_only(self):
-        datapipe_bg = PipeToReconOrder(type="Test", sample_type='BG')
-        self.processor_bg = ReconOrder()
-        self.processor_bg.frames = 5
-        self.processor_bg.compute_inst_matrix()
-        datapipe_bg.set_processor(self.processor_bg)
-        # datapipe_bg.compute_inst_matrix()
-        datapipe_bg.run_reconstruction()
-        return datapipe_bg
 
     def test_mse_Itrans(self):
         self.construct_all()
-        self.assertLessEqual(mse(self.processor.I_trans, cv2.imread(self.target_ITrans, -1)), 100000)
+        self.assertLessEqual(mse(self.I_trans, cv2.imread(self.target_ITrans, -1)), 100000)
 
     def test_mse_retard(self):
         self.construct_all()
-        self.assertLessEqual(mse(self.processor.retard, cv2.imread(self.target_retard, -1)), 100000)
+        self.assertLessEqual(mse(self.retard, cv2.imread(self.target_retard, -1)), 100000)
 
     def test_mse_orientation(self):
         self.construct_all()
-        self.assertLessEqual(mse(self.processor.azimuth_degree, cv2.imread(self.target_Orientation, -1)), 100000)
+        self.assertLessEqual(mse(self.azimuth, cv2.imread(self.target_Orientation, -1)), 100000)
 
     def test_mse_scattering(self):
         self.construct_all()
-        self.assertLessEqual(mse(self.processor.scattering, cv2.imread(self.target_Scattering, -1)), 100000)
-
-    def test_mse_ReuseBackground(self):
-        bg_pipe = self.construct_BG_only()
-        datapipe1 = PipeToReconOrder(type="Test", sample_type="Sample1")
-        datapipe2 = PipeToReconOrder(type="Test", sample_type="Sample2")
-        processor1 = ReconOrder()
-        processor2 = ReconOrder()
-        processor1.frames = 5
-        processor2.frames = 5
-
-        processor1.compute_inst_matrix()
-
-        datapipe1.set_processor(processor1)
-        datapipe2.set_processor(processor2)
-        datapipe1.compute_inst_matrix()
-        datapipe2.compute_inst_matrix()
-
-        datapipe1.run_reconstruction_BG_correction(bg_pipe.get_processor())
-        datapipe2.run_reconstruction_BG_correction(bg_pipe.get_processor())
+        self.assertLessEqual(mse(1-self.polarization, cv2.imread(self.target_Scattering, -1)), 100000)
 
 
 if __name__ == '__main__':
