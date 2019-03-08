e9d6ec5d2f1ad8fbf8bd1a513aab0430b76853e3
sguo <symguo@gmail.com>
2019-03-07 16:34:22 -0800

    Added the option to separate positions for the output

--- utils/imgIO.py
+++ utils/imgIO.py
@@ -190,14 +190,18 @@ def sort_pol_channels(img_pol):
         img_pol = np.stack((I_ext, I_0, I_45, I_90, I_135))  # order the channel following stokes calculus convention
     return img_pol
 
-def exportImg(img_io, imgDict):
+def exportImg(img_io, imgDict, separate_pos=True):
     tIdx = img_io.tIdx
     zIdx = img_io.zIdx
     posIdx = img_io.posIdx
     pos_name = img_io.pos_name
+    if separate_pos:
+        output_path = os.path.join(img_io.ImgOutPath, pos_name)
+    else:
+        output_path = img_io.ImgOutPath
     for tiffName in img_io.chNames:
         fileName = 'img_'+tiffName+'_t%03d_p%03d_z%03d.tif'%(tIdx, posIdx, zIdx)
         if len(imgDict[tiffName].shape)<3:
-            cv2.imwrite(os.path.join(os.path.join(img_io.ImgOutPath, pos_name), fileName), imgDict[tiffName])
+            cv2.imwrite(os.path.join(output_path, fileName), imgDict[tiffName])
         else:
-            cv2.imwrite(os.path.join(os.path.join(img_io.ImgOutPath, pos_name), fileName), cv2.cvtColor(imgDict[tiffName], cv2.COLOR_RGB2BGR))
+            cv2.imwrite(os.path.join(output_path, pos_name), fileName), cv2.cvtColor(imgDict[tiffName], cv2.COLOR_RGB2BGR)
