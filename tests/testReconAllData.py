d1f8b194b085c314f10ccf3a0c40848576249cd5
Syuan-Ming Guo <symguo@gmail.com>
2019-04-10 13:24:18 -0700

    fix for mousebrainslice test

--- tests/testReconAllData.py
+++ tests/testReconAllData.py
@@ -1,6 +1,6 @@
 from workflow.runReconstruction import runReconstruction
 
-configfiles = ['config/config_u2os_cells.yml','config/config_kidneytissue.yml']
+configfiles = ['config/mousebrainslice.yml', 'config/config_u2os_cells.yml']
 #TODO: analysis runs fine on glass beads and u2os cells, but not on Kidney Tissue data and Mouse Brain slice. Needs debugging.
 
 if __name__ == '__main__':
