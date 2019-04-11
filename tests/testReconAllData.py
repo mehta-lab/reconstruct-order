846394746c27359d4a36a5c9fc7828e8ac095a87
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2019-04-10 17:03:16 -0700

    Move runReconstruction to base dir
    Former-commit-id: ebff754dc55ec39f25b7578a1bf0d5365a325204

--- tests/testReconAllData.py
+++ tests/testReconAllData.py
@@ -1,4 +1,4 @@
-from workflow.runReconstruction import runReconstruction
+from runReconstruction import runReconstruction
 
 configfiles = ['config/mousebrainslice.yml', 'config/config_u2os_cells.yml']
 #TODO: analysis runs fine on glass beads and u2os cells, but not on Kidney Tissue data and Mouse Brain slice. Needs debugging.
