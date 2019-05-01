e534d0f3119167992f5ed26a352c3a30d0fb9d61
Bryant <bchhun@gmail.com>
2019-05-01 12:53:48 -0700

    moved sphinx docs for github-pages
    Former-commit-id: d4a6ac864b7a4f58f0d96d8bc92ecd80fa85a23e [formerly 79ca6eda1c343198a6ceac3cd6b180fb67eec247]
    Former-commit-id: 9c83d04a0768ebccdb5b5ef7d3be1e412ec846f8

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
         long_description_content_type=LONG_DESCRIPTION_content_type,
         license=LICENSE,
         download_url=DOWNLOAD_URL,
-        version="0.0.42",
+        version="0.0.1",
         classifiers=CLASSIFIERS,
         install_requires=INSTALL_REQUIRES,
         requires=REQUIRES,
@@ -64,5 +64,4 @@ if __name__ == '__main__':
         entry_points={
             'console_scripts': ['runReconstruction=ReconstructOrder.cli_module:main']
         }
-        # scripts=['cli_module.py']
     )
