b308a8afb4bb0b740e4d75f958caa99c3b74e8cb
Shalin Mehta <2934183+mattersoflight@users.noreply.github.com>
2019-09-07 19:38:11 -0700

    add functions to plot polarization ellipse of a stokes vector and a test notebook

--- simulations/mueller_matrices.py
+++ simulations/mueller_matrices.py
@@ -1,12 +1,81 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May 24 17:49:42 2019
-
-@author: ivan.ivanov
-"""
 
 import sympy as sp
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def stokes2ellipse(S):
+    """
+    Compute ellipticity and orientation of a polarization state specified by four Stokes parameters.
+
+    :param S: Stokes vector
+    :return: ellipticity: -1,...,0,...,1 for left circular, linear,  and right circular states
+    :return: orientation: Orientation of polarization ellipse in radian.
+    """
+    s1Norm = S[1] / S[0]
+    s2Norm = S[2] / S[0]
+    s3Norm = S[3] / S[0]
+    linearity = np.sqrt(s1Norm ** 2 + s2Norm ** 2)
+    if linearity:
+        ellipseAngle = np.arctan2(s3Norm, linearity)
+        ellipt = np.sin(ellipseAngle)
+
+    else:
+        ellipt = np.sign(s3Norm)
+
+    orient = 0.5 * np.arctan2(s2Norm, s1Norm)
+    return (ellipt, orient)
+
+
+def plotEllipse(ellipt, orient, size=5, axes=None):
+    def add_arrow(line, size=20, color='k'):
+        """
+        add an arrow to a line.
+
+        line:       Line2D object
+        position:   x-position of the arrow. If None, mean of xdata is taken
+        direction:  'left' or 'right'
+        size:       size of the arrow in fontsize points
+        color:      if None, line color is taken.
+
+        adapted from: https://stackoverflow.com/questions/34017866/arrow-on-a-line-plot-with-matplotlib
+        """
+        if color is None:
+            color = line.get_color()
+
+        xdata = line.get_xdata()
+        ydata = line.get_ydata()
+
+        position = xdata.mean()
+        # find closest index
+        start_ind = np.argmin(np.absolute(xdata - position))
+        end_ind = start_ind - 1
+
+        line.axes.annotate('',
+                           xytext=(xdata[start_ind], ydata[start_ind]),
+                           xy=(xdata[end_ind], ydata[end_ind]),
+                           arrowprops=dict(arrowstyle="->", color=color),
+                           size=size)
+
+    theta = np.linspace(0, 2 * np.pi, 360)
+    rotmat = [[np.cos(orient), -np.sin(orient)], [np.sin(orient), np.cos(orient)]]
+
+    x = np.cos(theta)
+    y = ellipt * np.sin(theta)
+    (xr, yr) = np.matmul(rotmat, [x.flatten(1), y.flatten(1)])
+
+    if axes == None:
+        plt.figure(figsize=[size, size])
+        axes = plt.axes()
+    else:
+        plt.sca(axes)
+    axes.axis('square')
+    line = plt.plot(xr, yr)[0]
+    add_arrow(line)
+    plt.xlim(-1, 1)
+    plt.ylim(-1, 1)
+
 
 def M_LinearPolarizer(theta=0):
     """
